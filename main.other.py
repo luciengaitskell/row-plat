fb167e449790f1d952fba8d2901cce5615dc7dc2
Lucien Gaitskell <lucieng@gaitskell.com>
2019-07-24 15:45:57 -0400

    Add bno055 testing main scripts

---
+++ main.other.py
@@ -0,0 +1,25 @@
+# bno055_test.py Simple test program for MicroPython bno055 driver
+
+# Copyright (c) Peter Hinch 2019
+# Released under the MIT licence.
+
+from machine import I2C, Pin
+import time
+from lib.other.bno055 import *
+
+i2c = I2C(0, scl=Pin(22, mode=Pin.PULL_UP), sda=Pin(23, mode=Pin.PULL_UP), freq=100000)
+
+imu = BNO055(i2c)
+calibrated = False
+while True:
+    time.sleep(1)
+    if not calibrated:
+        calibrated = imu.calibrated()
+        print('Calibration required: sys {} gyro {} accel {} mag {}'.format(*imu.cal_status()))
+    print('Temperature {}°C'.format(imu.temperature()))
+    print('Mag       x {:5.0f}    y {:5.0f}     z {:5.0f}'.format(*imu.mag()))
+    print('Gyro      x {:5.0f}    y {:5.0f}     z {:5.0f}'.format(*imu.gyro()))
+    print('Accel     x {:5.1f}    y {:5.1f}     z {:5.1f}'.format(*imu.accel()))
+    print('Lin acc.  x {:5.1f}    y {:5.1f}     z {:5.1f}'.format(*imu.lin_acc()))
+    print('Gravity   x {:5.1f}    y {:5.1f}     z {:5.1f}'.format(*imu.gravity()))
+    print('Heading     {:4.0f} roll {:4.0f} pitch {:4.0f}'.format(*imu.euler()))
