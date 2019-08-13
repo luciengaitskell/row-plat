833ca833a881026df133fc3d482825ee85827fa7
Lucien Gaitskell <lucieng@gaitskell.com>
2019-08-13 12:37:47 -0400

    Update recv main code for new hardware

--- recv_main.py
+++ recv_main.py
@@ -11,13 +11,13 @@ from devices.ssd1306 import SSD1306_I2C
 from devices.rfm69 import RFM69
 
 
-LOOP_PERIOD = 200
+LOOP_PERIOD = 50
 
 # Chip communication setup:
 i2c_bus = I2C(0, scl=Pin(22, mode=Pin.PULL_UP), sda=Pin(23, mode=Pin.PULL_UP))
 
 # Device setup:
-display = SSD1306_I2C(128, 32, i2c_bus)
+display = SSD1306_I2C(128, 64, i2c_bus, 0x3D)
 
 radio = RFM69(12, 27, baudrate=5000000)
 
@@ -29,10 +29,20 @@ gns_data = MicropyGPS(location_formatting='dd')
 
 imu = bno055.BNO055(i2c_bus)'''
 
+# Buttons -- NOTE: Pull Ups do not seem to work on these pins: 36, 39, 34; used external pull-up
+B_UP = Pin(36, Pin.IN, Pin.PULL_UP)
+B_DOWN = Pin(4, Pin.IN, Pin.PULL_UP)
+B_LEFT = Pin(39, Pin.IN, Pin.PULL_UP)
+B_RIGHT = Pin(34, Pin.IN, Pin.PULL_UP)
+
+B_ENB = Pin(14, Pin.IN, Pin.PULL_UP)
+B_ALT = Pin(32, Pin.IN, Pin.PULL_UP)
+
+
 # Shared variables:
 running = True
 ang = None
-pos = None
+pos = (0, b'-', 0, b'-')
 
 
 '''
@@ -56,12 +66,12 @@ def read_bno055():
 
 def recv_data():
     global pos, ang
-    data = radio.receive()  # Data seems to be incorrect after send -- need to revise encoding for errors?
-    ret = ustruct.unpack("dfsfs", data)
-    ang = float(ret[0])
-    pos = ret[1:]
+    data = radio.receive(timeout=0.1)  # Data seems to be incorrect after send -- need to revise encoding for errors?
+    if isinstance(data, bytes):
+        ret = ustruct.unpack("dfsfs", data)
+        ang = float(ret[0])
+        pos = ret[1:]
     print(data)
-
 
 def update_display():
     global pos
@@ -73,6 +83,15 @@ def update_display():
         "{:.2f} {}, {:.2f} {}".format(
             pos[0], pos[1].decode(), pos[2], pos[3].decode()),
         0, 14)
+
+    display.text("{}".format(B_UP.value()), 16, 28)
+
+    display.text("{} | {}".format(B_LEFT.value(), B_RIGHT.value()), 0, 42)
+
+    display.text("{}".format(B_DOWN.value()), 16, 56)
+
+    display.text("{} | {}".format(B_ENB.value(), B_ALT.value()), 64, 42)
+
     display.show()
 
 
@@ -88,9 +107,10 @@ try:
         update_display()
 
         # Handle loop sleep, based on elapsed time:
-        sleep = time.ticks_diff(time.ticks_ms(), loop_start)
+        t_elapsed = time.ticks_diff(time.ticks_ms(), loop_start)
+        sleep = LOOP_PERIOD - t_elapsed
         if sleep > 0:
-            time.sleep_ms(LOOP_PERIOD - sleep)
+            time.sleep_ms(sleep)
         else:
             print("CLOCK STRETCHING")
 finally:
