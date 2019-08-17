3189cc37dd898482b845ccacfb5c799931a11c7a
Lucien Gaitskell <lucieng@gaitskell.com>
2019-08-17 11:12:37 -0400

    Add comments to platform

--- coproc/platform.py
+++ coproc/platform.py
@@ -9,23 +9,38 @@ from devices.ssd1306 import SSD1306_I2C
 
 class Platform:
     def __init__(self, id: str, display: bool=True):
-        self.running = False
+        """ Create platform object. """
+
+        self.running = False  # Status of platform. Used for concurrent threads
+
+        """
+        Interfaces/Buses:
+         - I2C
+        """
         self.i2c_bus = I2C(0, scl=Pin(22, mode=Pin.PULL_UP), sda=Pin(23, mode=Pin.PULL_UP))
 
-        self.c = CommsNode(id, None)
-        self.radio = RFM69(12, 27, baudrate=5000000)
+        """
+        Hardware:
+         - RFM69 Radio
+         - SSD1306 Display
+        """
+        self.radio = RFM69(12, 27, baudrate=int(2e6))  # NOTE: Bytes in communication became corrupted with baudrates
+        #   above 4 MHz (noticed unreliability at 4.4 MHz and full breakdown at 5 MHz)
         if display is True:
             self.display = SSD1306_I2C(128, 32, self.i2c_bus)
         else:
             self.display = None
 
-        """
+        """ Wrappers/Supporting Classes """
+        self.c = CommsNode(id, None)  # Handles messaging over radio
+
+        '''
         gns = XA1110(i2c=i2c_bus)
         gns.write(b'PMTK314,0,1,1,1,1,5,0,0,0,0,0,0,0,0,0,0,0,1,0')
         gns.read_all_data()  # Clear buffer so that we know that data that followings is current
         gns_data = MicropyGPS(location_formatting='dd')
 
-        imu = bno055.BNO055(i2c_bus)"""
+        imu = bno055.BNO055(i2c_bus)'''
 
     def setup(self):
         self.radio.init(915.0)
