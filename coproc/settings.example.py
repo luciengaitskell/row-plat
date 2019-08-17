2e38b62d49071f833590a28a8dd5544433762bbb
Lucien Gaitskell <lucieng@gaitskell.com>
2019-08-17 11:11:33 -0400

    Add comments to example settings file

--- coproc/settings.example.py
+++ coproc/settings.example.py
@@ -1,1 +1,6 @@
-MODE = None  # 'SEND' or 'RECV' or 'DEBUG'
+""" settings.py
+
+Copy this example and set values to configure runtime on specific deployment.
+"""
+
+MODE = str  # 'SEND' or 'RECV' or 'DEBUG'
